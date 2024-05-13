1e896be2556c02364de4b1837540fac71d75b944
Tarek <114747789+Tarek124@users.noreply.github.com>
2024-05-13 21:07:07 +0600

    jwt token and middleware done

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookiePerser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
 //middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookiePerser());
 
+//coustome middleware
+const verifyToken = (req, res, next) => {
+  const token = req.cookie.token;
+  if (!token) {
+    return res.status(401).send("unauthorized user");
+  }
+  try {
+    jwt.verify(token, secret_key, (err, decoded) => {
+      if (err) {
+        return res.status(401).send("unauthorized user");
+      }
+      req.user = decoded;
+    });
+    next();
+  } finally {
+  }
+};
+
+//Home route
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.83drhwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Secret key for Json Web Token
+const secret_key = process.env.SECRET_KEY;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.83drhwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,6 +61,54 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+    const jobCollection = client.db("assignment-11").collection("jobs");
+    app.get("/allJobs", async (req, res) => {
+      const result = await jobCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/jobdetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
+
+    //create json web token and set in cookie
+    app.post("/jsonwebtoken", (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, secret_key, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "lax",
+        })
+        .send("JWT created and set in cookie");
+    });
+
+    //clear cookie
+    app.get("/logout", (req, res) => {
+      // Clear cookie on the client-side
+      res.clearCookie("token").send("Logged out successfully");
+      console.log("Logged out successfully");
+    });
+    // Route for searching jobs by title
+    app.get("/jobs/search", async (req, res) => {
+      const { query } = req.query;
+      const regex = new RegExp(query, "i");
+      const searchQuery = { category: { $regex: regex } };
+      const searchQuery2 = { title: { $regex: regex } };
+
+      try {
+        const searchResults = await jobCollection
+          .find({ $or: [searchQuery, searchQuery2] })
+          .toArray();
+        res.send(searchResults);
+      } catch (error) {
+        console.error("Error searching jobs:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
   } finally {
     // Ensures that the client will close when you finish/error
   }
